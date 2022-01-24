27166ca0c75343c5da4df43ca2208150d2902a1e
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-23 22:16:22 -0500

    Todo's for dashboard-routes and home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -26,6 +26,16 @@ router.get('/post/:id', async (req, res) => {
       //  SECOND ARGUMENT IS AN OBJECT IN WHICH YOU USE PROPERTY 'INCLUDE' TO INCLUDE USER
       //  AND COMMENT
       // TODO: YOUR CODE HERE
+      req.params.id,
+      {
+        include: [
+          User,
+          {
+            model: Comment,
+            include: [User],
+          }
+        ]
+      }
     );
 
     if (postData) {
